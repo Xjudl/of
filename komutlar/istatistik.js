45fd024153f06e1e2af979389e632f85a4c1f64a
Xjudl <104034182+Xjudl@users.noreply.github.com>
2022-04-19 23:58:22 +0300

    TR
    TR

---
+++ komutlar/istatistik.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+
+exports.run = async (client, message, args) => {
+
+  
+const istatistikler = new Discord.MessageEmbed()
+ .addField(`:bar_chart: » Kanal Sayısı`, `${client.channels.cache.size  }`, true)
+ .addField(`:postbox: » Kullanıcı Sayısı`, `${client.users.cache.size}`, true)
+ .addField(`:envelope: » Sunucu Sayısı`, `${client.guilds.cache.size}`, true)
+ .addField(`» Linkler`, `[Destek Sunucusu](https://discord.gg/PPH44U8k4g)`, true)
+  message.channel.send(istatistikler)
+  
+  
+}
+  
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['stat'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'i',
+  description: 'Otorol sistemini ayarlamaya yarar.',
+  usage: '-otorol-ayarla @rol #kanal'
+}; 
